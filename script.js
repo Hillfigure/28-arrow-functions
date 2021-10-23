b10a215a06029c1d36bbbaefd8670b7bc46b8eb7
R van Bijnen <robvanbijnen@hotmail.com>
2021-10-23 11:56:04 +0200

    script added

---
+++ script.js
@@ -0,0 +1,11 @@
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+ikRockArrowFunctions()
+
+const fivePlusSeven = () => 5 + 7; 
+fivePlusSeven();
+
+const myFunction = (a,b) => a+b;
+
+const addFive = a => a+5;
+
+const createObject = () => ({greeting: "hoi"});
